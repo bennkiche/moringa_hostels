6e76ddfd24f402bb3156be286b2d74d82c325dce
lorna gathumbi <nyawiralornah@gmail.com>
2025-02-20 20:15:43 +0300

    make changes to models.py

--- models.py
+++ models.py
@@ -3,28 +3,28 @@ from sqlalchemy_serializer import Serializermixin
 
 db = SQLAlchemy()
 
-class Users(db.Model, Serializermixin):
-    __tablename__ = 'users'
+class User(db.Model, Serializermixin):
+    __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key = True, unique = True)
     name = db.Column(db.String(100), nullable = False)
     email = db.Column(db.String(100), nullable = False, unique = True)
     password = db.Column(db.String(100), nullable = False)
  
-    bookings = db.relationship('Bookings', backref = 'user', lazy = True)
-    accommodation = db.relationship('Accommodation', backref = 'user', lazy = True)
+    booking = db.relationship('Booking', backref = 'user', lazy = True)
+    accommodations = db.relationship('Accommodations', backref = 'user', lazy = True)
     user_verification = db.relationship('User_verification', backref = 'user', lazy = True)
     password_reset = db.relationship('Password_reset', backref = 'user', lazy = True)
    
 
-    serialize_rules = ('-bookings', '-accommodation.user' , '-user_verification.user', '-password_reset.user')
+    serialize_rules = ('-booking', '-accommodations.user' , '-user_verification.user', '-password_reset.user')
     
     def __repr__(self):
         return f"User('{self.name}', '{self.email}')"
     
     
-class Accommodation(db.Model, Serializermixin):
-    __tablename__ = 'accommodation'
+class Accommodations(db.Model, Serializermixin):
+    __tablename__ = 'accommodations'
 
     id = db.Column(db.Integer, primary_key = True, unique = True)
     name  = db.Column(db.String(100), nullable = False)
@@ -35,61 +35,61 @@ class Accommodation(db.Model, Serializermixin):
     availability = db.Column(db.String(50), nullable=False)
 
 
-    bookings = db.relationship('Bookings', backref = 'accommodation', lazy = True)
-    user = db.relationship('Users', backref = 'accommodation', lazy = True)
+    booking = db.relationship('Booking', backref = 'accommodations', lazy = True)
+    user = db.relationship('User', backref = 'accommodations', lazy = True)
 
 
-    serialize_rules = ('-user.accommodation', '-bookings')
+    serialize_rules = ('-user.accommodations', '-booking')
 
     def __repr__(self):
-        return f"Accommodation('{self.title}', '{self.user_id}')"
+        return f"Accommodations('{self.name}', '{self.user_id}')"
     
-class Bookings(db.Model, Serializermixin):
-    __tablename__ = 'bookings'
+class Booking(db.Model, Serializermixin):
+    __tablename__ = 'booking'
 
     id = db.Column(db.Integer, primary_key = True, unique = True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    accommodation_id = db.Column(db.Integer, db.ForeignKey('accommodation.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    accommodations_id = db.Column(db.Integer, db.ForeignKey('accommodations.id'), nullable=False)
     date_checkin = db.Column(db.DateTime, nullable=False)
     date_checkout = db.Column(db.DateTime, nullable=False)
 
-    user = db.relationship('Users', backref = 'bookings', lazy = True)
-    accommodation = db.relationship('Accommodation', backref = 'bookings', lazy = True)
-    payments = db.relationship('Payments', backref = 'bookings', lazy = True)
+    user = db.relationship('User', backref = 'booking', lazy = True)
+    accommodations = db.relationship('Accommodations', backref = 'booking', lazy = True)
+    payments = db.relationship('Payments', backref = 'booking', lazy = True)
 
-    serialize_rules = ('-user.bookings', '-payments', '-accommodation.bookings')
+    serialize_rules = ('-user.booking', '-payments', '-accommodations.booking')
 
 
     def __repr__(self):
-        return f"Bookings('{self.user_id}', '{self.accommodation_id}')"
+        return f"Booking('{self.user_id}', '{self.accommodations_id}')"
 
 class Payments(db.Model, Serializermixin):
     __tablename__ = 'payments'
 
     id = db.Column(db.Integer, primary_key = True, unique = True)
-    bookings_id = db.Column(db.Integer, db.ForeignKey('bookings.id'), nullable=False)
+    booking_id = db.Column(db.Integer, db.ForeignKey('booking.id'), nullable=False)
     payment_amount = db.Column(db.Integer, nullable=False)
     payment_date = db.Column(db.DateTime, nullable=False)
 
 
-    bookings = db.relationship('Bookings', backref = 'payments', lazy = True)
+    booking = db.relationship('Booking', backref = 'payments', lazy = True)
 
     
-    serialize_rules = ('-bookings')
+    serialize_rules = ('-booking')
 
 
     def __repr__(self):
-        return f"Payment('{self.bookings_id}', '{self.payment_amount}')"
+        return f"Payment('{self.booking_id}', '{self.payment_amount}')"
     
 class User_verification(db.Model, Serializermixin):
     __tablename__ = 'user_verification'
 
     id = db.Column(db.Integer, primary_key = True, unique = True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     status = db.Column(db.String(50), nullable=False)
     
 
-    user = db.relationship('Users', backref = 'user_verification', lazy = True)
+    user = db.relationship('User', backref = 'user_verification', lazy = True)
 
     serialize_rules =('-user.user_verification',)
 
@@ -99,16 +99,16 @@ class Password_reset(db.Model, Serializermixin):
     __tablename__ = 'password_reset'
 
     id = db.Column(db.Integer, primary_key = True, unique = True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     reset_token = db.Column(db.String(100), nullable=False)
     reset_expires = db.Column(db.DateTime, nullable=False)
     
 
-    user = db.relationship('Users', backref = 'password_reset', lazy = True)
+    user = db.relationship('User', backref = 'password_reset', lazy = True)
 
     serialize_rules = ('user.password_reset',)
 
 
 
     def __repr__(self):
-        return f"Password_reset('{self.user_id}', '{self.password}')"
+        return f"Password_reset('{self.user_id}', '{self.reset_token}')"
